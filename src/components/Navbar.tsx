7f8e9ed96b68a721b62a9c473a62e32d9d008e7d
Vaishnavi Patil <vaishnavip276@gmail.com>
2024-10-28 19:06:45 +0530

    commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,11 +15,23 @@ const Navbar = () => {
         </div>
         <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
           <Image src="/announcement.png" alt="" width={20} height={20} />
+          <div className="absolute -top-3 -right-3 w-5 h-5 flex items-center justify-center">
+            1
+          </div>
         </div>
         <div className="flex flex-col">
           <span className="text-xs leading-3 font-medium">Vaishnavi Patil</span>
-          <span className="text-[10px] text-gray-500">FrontendDev</span>
+          <span className="text-[10px] text-gray-500 text-right">
+            FrontendDev
+          </span>
         </div>
+        <Image
+          src="/avatar.png"
+          alt=""
+          height={36}
+          width={36}
+          className="rounded-full"
+        />
       </div>
     </div>
   );
