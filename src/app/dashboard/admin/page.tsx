45cb9c004f7f666c3e2c1e1a9d5a258241f3c596
Vaishnavi Patil <vaishnavip276@gmail.com>
2024-10-28 20:36:21 +0530

    commit

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,5 +1,33 @@
+import UserCard from "../../../components/UserCard";
+
 const AdminPage = () => {
-  return <div>page</div>;
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* left */}
+      <div className="w-full lg:w-2/3">
+        {/* user cards */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
+        </div>
+
+        {/* middle charts */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* count chart */}
+          <div className="w-full lg:w-1/3"></div>
+          {/* attendance chart */}
+          <div className="w-full lg:w-2/3"></div>
+        </div>
+
+        {/* bottom charts */}
+        <div className=""></div>
+      </div>
+      {/* right */}
+      <div className="w-full lg:w-1/3">right</div>
+    </div>
+  );
 };
 
 export default AdminPage;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,18 +4,22 @@ const Navbar = () => {
   return (
     <div className="flex items-center justify-between p-4">
       {/* Search bar */}
-      <div className="hidden md:flex">
+      <div className="hidden md:flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
         <Image src="/search.png" alt="" height={14} width={14} />
-        <input type="text" placeholder="Search" />
+        <input
+          type="text"
+          placeholder="Search..."
+          className="w-[200px] p-2 bg-transparent outline-none"
+        />
       </div>
       {/* Icons and User */}
-      <div className="flex items-center gap-6">
+      <div className="flex items-center gap-6 justify-end w-full">
         <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
           <Image src="/message.png" alt="" width={20} height={20} />
         </div>
-        <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
+        <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer relative">
           <Image src="/announcement.png" alt="" width={20} height={20} />
-          <div className="absolute -top-3 -right-3 w-5 h-5 flex items-center justify-center">
+          <div className="absolute -top-3 -right-3 w-5 h-5 flex items-center justify-center bg-purple-500 text-white rounded-full text-xs">
             1
           </div>
         </div>
