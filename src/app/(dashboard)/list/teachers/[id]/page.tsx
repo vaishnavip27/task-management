25d5aea2976dc10131bc8600369b4f0fc21fcf03
Vaishnavi Patil <vaishnavip276@gmail.com>
2024-10-31 12:42:02 +0530

    commit

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -3,6 +3,7 @@ import BigCalendar from "../../../../../components/BigCalender";
 import { role } from "../../../../../lib/data";
 import Image from "next/image";
 import Link from "next/link";
+import FormModal from "../../../../../components/FormModal"
 import Performance from "../../../../../components/Performance";
 
 const SingleTeacherPage = () => {
@@ -26,6 +27,20 @@ const SingleTeacherPage = () => {
             <div className="w-2/3 flex flex-col justify-between gap-4">
               <div className="flex items-center gap-4">
                 <h1 className="text-xl font-semibold">Leonard Snyder</h1>
+                <FormModal table="teacher" type="update" data={{
+                  id:1,
+                  username:"Vaishnavi",
+                  email:"vaishnavip276@gmail.com",
+                  password:"password",
+                  firstName:"Vaishnavi",
+                  lastName:"Patil",
+                  phone:"+1 234 567 789",
+                  address:"123 Main St, Aniynam, USA",
+                  bloodType:"A+",
+                  dateOfBirth:"2000-01-01",
+                  sex:"male",
+                  img:"https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=1200",
+                }} />
               </div>
               <p className="text-sm text-gray-500">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit.
